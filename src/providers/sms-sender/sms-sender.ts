2cb2c2b0a51254897cffa6a3f24571d4e5c43fd2
Jeff930 <jefferson30.jdg@gmail.com>
2018-04-24 00:56:06 +0800

    Added a provider for sms sending

---
+++ src/providers/sms-sender/sms-sender.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { SMS } from '@ionic-native/sms';
+
+/*
+  Generated class for the SmsSenderProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class SmsSenderProvider {
+
+  constructor(private sms: SMS) {}
+
+  public Send(){
+    this.sms.send('09295868987', 'Testing');
+  }
+
+}
