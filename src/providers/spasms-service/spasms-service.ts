e1eb0bf2453a50b5649aea6db6732663835c2fe4
Jeff930 <jefferson30.jdg@gmail.com>
2018-05-07 00:05:58 +0800

    Merged existing providers to SPASMS Service Provider

--- src/providers/spasms-service/spasms-service.ts
+++ src/providers/spasms-service/spasms-service.ts
@@ -1,5 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ModalController, NavParams } from 'ionic-angular';
+import { CompleteModal } from '../../pages/complete/complete';
+import 'rxjs/add/operator/map';
+import { SmsSenderProvider } from '../../providers/sms-sender/sms-sender';
+import { PageServiceProvider } from '../../providers/pageservice/pageservice';
+import { SMS } from '@ionic-native/sms';
 
 /*
   Generated class for the SpasmsServiceProvider provider.
@@ -10,8 +16,67 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class SpasmsServiceProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello SpasmsServiceProvider Provider');
+  constructor(public modalCtrl: ModalController,public http: HttpClient,private sms: SMS) {}
+ 
+  public showSending(){
+    document.getElementById('standby').style.display = "none";
+    document.getElementById('sending').style.display = "block";
+  }
+
+  public presentCompleteModal() {
+    let completeModal = this.modalCtrl.create(CompleteModal);
+    completeModal.present();
+  }
+
+  showFinished(){
+    document.getElementById('error').style.display = "none";
+    document.getElementById('stopped').style.display ="none";
+    document.getElementById('sending').style.display = "none";
+    document.getElementById('finished').style.display ="block";    
+  }
+
+  showStandby(){
+    document.getElementById('error').style.display = "none";
+    document.getElementById('finished').style.display ="none";
+    document.getElementById('stopped').style.display ="none";
+    document.getElementById('standby').style.display = "block";
+  }
+
+  showStopped(){
+    document.getElementById('sending').style.display = "none";
+    document.getElementById('standby').style.display = "none";
+    document.getElementById('stopped').style.display = "block";
+  }
+
+  showError(){
+    document.getElementById('sending').style.display = "none";
+    document.getElementById('finished').style.display = "none";
+    document.getElementById('standby').style.display = "none";
+    document.getElementById('stopped').style.display = "none";
+    document.getElementById('error').style.display = "block";
+    setTimeout(() => { 
+      this.showStandby();}, 5000); 
+  }
+
+  getRemoteData(){
+    this.http.get('http://api.randomuser.me/?results=1&noinfo').subscribe(data => {
+        alert(JSON.stringify(data));
+    },error => this.showError());
+  }
+  
+  checkRemoteData(data){
+    if (data.toString==""){
+      setTimeout(() => { 
+        console.log('nothing new');
+        this.getRemoteData();}, 4000); 
+    }else{
+      this.Send(data.toString);
+      this.showSending();
+    }
+  }
+
+  public Send(data){
+    this.sms.send('09295868987', data);
   }
 
 }
