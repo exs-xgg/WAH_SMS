d39c07ef3ca849814f743baa4e2bf822c08b77cc
Jeff930 <jefferson30.jdg@gmail.com>
2018-05-08 01:48:54 +0800

    Fixed a bug that prevents sent, failed and total messages from being shown

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component} from '@angular/core';
-import { ModalController } from 'ionic-angular';
+import { ModalController,NavController } from 'ionic-angular';
 import { SpasmsServiceProvider } from '../../providers/spasms-service/spasms-service';
 import { SMS } from '@ionic-native/sms';
 import { HttpClient } from '@angular/common/http';
@@ -12,21 +12,18 @@ import { BackgroundMode } from '@ionic-native/background-mode';
 
 export class HomePage{
 
-  constructor(public modalCtrl: ModalController,public sms: SMS, public backgroundMode: BackgroundMode,public http: HttpClient) {
+  constructor(public modalCtrl: ModalController,
+              public sms: SMS, 
+              public backgroundMode: BackgroundMode,
+              public http: HttpClient,
+              public navCtrl: NavController,
+            ) {
   }
 
-  service = new SpasmsServiceProvider(this.modalCtrl,this.http,this.sms);
+  service = new SpasmsServiceProvider(this.modalCtrl,this.http,this.sms,this.navCtrl);
 
   ngOnInit(){
     this.backgroundMode.enable();
     this.service.showStandby();
-  }
-
-  onBack(){
-    this.service.showStandby();
-  }
-
-  onViewResults(){
-    this.service.presentCompleteModal();
   }
 }
