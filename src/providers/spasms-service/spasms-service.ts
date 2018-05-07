821d3471e2fbe9b3278dc9a0ad2e23f25b03c719
Jeff930 <jefferson30.jdg@gmail.com>
2018-05-08 01:50:03 +0800

    Removed the modal for viewing results and integrated it to the showFinished()

--- src/providers/spasms-service/spasms-service.ts
+++ src/providers/spasms-service/spasms-service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ModalController, NavParams } from 'ionic-angular';
+import { ModalController, NavParams,NavController } from 'ionic-angular';
 import { CompleteModal } from '../../pages/complete/complete';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toPromise';
@@ -18,7 +18,8 @@ export class SpasmsServiceProvider {
   Json;
   LGU_name="";
   total_Messages=0;
-  failed_Message=0;
+  total_Daily_Messages=0;
+  failed_Messages=0;
   sent_Messages=0;
   finished_Messages=0;
   id: any = [];
@@ -27,15 +28,16 @@ export class SpasmsServiceProvider {
 
   constructor(public modalCtrl: ModalController,
               public http: HttpClient,
-              private sms: SMS) {}
+              private sms: SMS,
+              private navCtrl:NavController
+            ) {}
  
-  public showSending(){
+  showSending(){
     document.getElementById('standby').style.display = "none";
     document.getElementById('sending').style.display = "block";
-    
   }
 
-  public presentCompleteModal() {
+  presentCompleteModal() {
     let completeModal = this.modalCtrl.create(CompleteModal);
     completeModal.present();
   }
@@ -48,6 +50,9 @@ export class SpasmsServiceProvider {
   }
 
   showStandby(){
+    this.total_Daily_Messages=0;
+    this.failed_Messages=0;
+    this.sent_Messages=0;
     document.getElementById('error').style.display = "none";
     document.getElementById('finished').style.display ="none";
     document.getElementById('stopped').style.display ="none";
@@ -81,8 +86,7 @@ export class SpasmsServiceProvider {
   }
 
   getMessages(){
-    
-    this.http.get('http://192.168.1.127/api/spasms/showSms').toPromise()
+    this.http.get('../../assets/sample.json').toPromise()
     .then((data:any)=> { 
       var message_quantity=[];
       message_quantity=data;
@@ -93,22 +97,9 @@ export class SpasmsServiceProvider {
         this.message[k]=i.msg;
         k++;
     };   
+    this.showSending();
     this.Sender();
-    console.log("1");
-    //this.showSending();
   })
-      // var message_quantity=[];
-      // for (var i of json) {
-      //   message_quantity.push(i.quantity_produced);
-      // }
-    //   if (data="[]"){
-    //     setTimeout(() => { 
-    //       },15000)
-    //   }else{
-    //     this.showSending(data);
-    //   }
-    // },error =>  setTimeout(() => { 
-    //   this.showError();},5000))
   }
 
   getLogs(){
@@ -122,17 +113,19 @@ export class SpasmsServiceProvider {
   Sender(){
     var phoneNumber ="";
     var Message="";
-    console.log("2");
-    for (let i=0; i<=this.id.length; i++) {
-      console.log(i);
-      console.log(this.receiver[i]);
+    this.total_Messages=this.id.length;
+    for (let i=0; i<this.total_Messages; i++) {
       phoneNumber=this.receiver[i];
       Message=this.message[i];
-      this.sms.send(phoneNumber, Message);
-  };   
-
-    
-    
+      this.sms.send(phoneNumber,Message).then((result) => {
+        this.sent_Messages++;
+        }, (error) => {
+        this.failed_Messages++;
+        console.log("error",this.failed_Messages);
+        })
+        this.finished_Messages++;
+      }
+    this.showFinished();   
   }
 
 }
