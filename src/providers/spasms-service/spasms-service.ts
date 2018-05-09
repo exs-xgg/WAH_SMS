0859dbb44d1921a08d7d9fd7d5780ecf7f6694d1
Jeff930 <jefferson30.jdg@gmail.com>
2018-05-09 22:50:09 +0800

    responsive font size based on number of characters on RHU name

--- src/providers/spasms-service/spasms-service.ts
+++ src/providers/spasms-service/spasms-service.ts
@@ -35,7 +35,14 @@ export class SpasmsServiceProvider {
   showSending(){
     document.getElementById('standby').style.display = "none";
     document.getElementById('sending').style.display = "block";
+    // setTimeout(() => { 
+    //   for (; this.finished_Messages<=this.total_Messages;) {
+    //     if (this.finished_Messages==this.total_Messages){
+    //       this.showFinished();
+    //   }else{}
+    // };}, 2000); 
   }
+    
 
   presentCompleteModal() {
     let completeModal = this.modalCtrl.create(CompleteModal);
@@ -51,13 +58,15 @@ export class SpasmsServiceProvider {
 
   showStandby(){
     this.total_Daily_Messages=0;
-    this.failed_Messages=0;
-    this.sent_Messages=0;
+    // this.failed_Messages=0;
+    // this.sent_Messages=0;
+    // this.finished_Messages=0;
+    this.total_Messages=0;
     document.getElementById('error').style.display = "none";
     document.getElementById('finished').style.display ="none";
     document.getElementById('stopped').style.display ="none";
     document.getElementById('standby').style.display = "block";
-    //this.getMessages();
+    this.getMessages();
   }
 
   showStopped(){
@@ -77,16 +86,29 @@ export class SpasmsServiceProvider {
   }
 
   getLGU(){
-    this.http.get('http://192.168.1.129/api/spasms/getRHU').toPromise()
+    this.http.get('../../assets/RHU.json').toPromise()
+    //this.http.get('http://192.168.1.129/api/spasms/getRHU').toPromise()
     .then((data:any)=> { 
-      var i;
-      this.LGU_name=i.data;
-     
-  })}
+      var LGU=[];
+      LGU=data;
+      for (var i of LGU) {
+        this.LGU_name=i.hfhudname;
+    };
+    if (this.LGU_name.length<=25){
+      document.getElementById('LGU').style.fontSize = "6.2vw";
+    }else if (this.LGU_name.length<=30){
+            document.getElementById('LGU').style.fontSize = "5.4vw";
+          }else if (this.LGU_name.length<=35){
+                  document.getElementById('LGU').style.fontSize = "4.6vw";
+                }else if (this.LGU_name.length<=40){
+                        document.getElementById('LGU').style.fontSize = "3.8vw";
+                      }else{
+                        document.getElementById('LGU').style.fontSize = "3.2vw";}
+  },error=>alert("Cannot retrieve RHU name"))}
 
   getMessages(){
-    //this.http.get('../../assets/sample.json').toPromise()
-    this.http.get('http://192.168.1.129/api/spasms/showSms').toPromise()
+    this.http.get('../../assets/sample.json').toPromise()
+    //this.http.get('http://192.168.1.129/api/spasms/showSms').toPromise()
     .then((data:any)=> { 
       var message_quantity=[];
       message_quantity=data;
@@ -97,7 +119,6 @@ export class SpasmsServiceProvider {
         this.message[k]=i.msg;
         k++;
     };   
-    this.showSending();
     this.Sender();
   })
   }
@@ -120,18 +141,24 @@ export class SpasmsServiceProvider {
       Message=this.message[i];
       this.sms.send(phoneNumber,Message).then((result) => {
         this.sent_Messages++;
+        this.finished_Messages++;
         status = "s";
         }, (error) => {
         this.failed_Messages++;
+        this.finished_Messages++;
         status = "x";
         })
-        this.finished_Messages++;
-        console.log('http://192.168.1.129/api/spasms/updateStats/'+this.id[i]+'/'+status);
-        this.http.get('http://192.168.1.129/api/spasms/updateStats/'+this.id[i]+'/'+status).toPromise()
-        .then((data:any)=> { 
-      })
+        console.log(this.sent_Messages);
+        console.log(this.failed_Messages);
+        console.log(this.total_Messages);
+        console.log(this.finished_Messages);     
+        console.log(this.failed_Messages);  
+      //  console.log('http://192.168.1.129/api/spasms/updateStats/'+this.id[i]+'/'+status);
+      //   this.http.get('http://192.168.1.129/api/spasms/updateStats/'+this.id[i]+'/'+status).toPromise()
+      //   .then((data:any)=> { 
+      // })
       }
-    this.showFinished();   
+      this.showSending();
   }
 
 }
